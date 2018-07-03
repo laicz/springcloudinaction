c78d684eb825cdd988eeeb88fc3f7707ac9f0ab7
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-07-03 08:12:13 +0800

    Initial commit

---
+++ licensing-service/src/main/java/com/zhou/licensingservice/LicensingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zhou.licensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+}
