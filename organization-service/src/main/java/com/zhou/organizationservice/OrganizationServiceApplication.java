8445f934c0726fa837c725f0745b8776798701fe
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-07-05 23:14:21 +0800

    eureka服务器搭建和服务注册

---
+++ organization-service/src/main/java/com/zhou/organizationservice/OrganizationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zhou.organizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServiceApplication.class, args);
+	}
+}
