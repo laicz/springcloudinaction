5d72baaf31b4dd746b0d9e00e347d09071b1a8bb
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-07-15 22:51:18 +0800

    设置网关zuul的网络超时时间

--- organization-service/src/main/java/com/zhou/organizationservice/web/OrganizationController.java
+++ organization-service/src/main/java/com/zhou/organizationservice/web/OrganizationController.java
@@ -7,16 +7,19 @@ package com.zhou.organizationservice.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 2018/7/15  16:05
  * created by zhoumb
  */
 @RestController
-@RequestMapping(value = "/organization")
+@RequestMapping(value = "/open")
 public class OrganizationController {
 
     @RequestMapping(value = "/ping")
-    public String ping() {
+    public String ping() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
         return "Hello World ! organization";
     }
 }
